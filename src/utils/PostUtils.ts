da27a02120b7c26f0aeaa40beb2d1896be24b503
hamdan266 <sehahh26@gmail.com>
2024-09-19 13:33:34 +0700

    create latest post section

---
+++ src/utils/PostUtils.ts
@@ -0,0 +1,9 @@
+export const getPostColorFromCategory = (category: string) => {
+    switch (category) {
+        case "Sustainability": return 'lime';
+        case "Innovation": return 'cyan';
+        case "Security": return 'violet';
+        default: return 'fuchsia';
+
+    }
+}
