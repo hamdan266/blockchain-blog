2ef78caf7c1045b5698dade23c2da5f4f5d75035
hamdan266 <sehahh26@gmail.com>
2024-09-20 00:25:10 +0700

    call to action animation

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,26 +1,51 @@
 import { CutCornerButton } from "../components/CutCornerButton";
 import { Hexagon } from "../components/Hexagon";
 import { Circle } from "../components/Circle";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export const CallToAction = () => {
+    const sectionRef = useRef(null);
+
+    const { scrollYProgress } = useScroll({
+        target: sectionRef,
+        offset: ['start end', 'end start']
+    });
+
+    const rotate = useTransform(scrollYProgress, [0, 1], [45, -45]);
+
     return (
-        <section className="py-60 overflow-hidden">
+        <section className="py-60 overflow-hidden" ref={sectionRef}>
             <div className="container">
                 <div className="relative">
                     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                        <Hexagon className="size-[700px]" />
+                        <Hexagon size={700} />
                     </div>
                     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                        <Hexagon className="size-[1100px]" />
+                        <Hexagon size={1100} reverse duration={50} />
                     </div>
                     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                        <Circle className="absolute left-0 -top-[400px]">
-                            <img src="/assets/images/cuboid.png" alt="cuboid 3D image" className="size-[140px]" />
+                        <Circle className="absolute left-0 -top-[400px]" animate>
+                            <motion.img
+                                src="/assets/images/cuboid.png"
+                                alt="cuboid 3D image"
+                                className="size-[140px]"
+                                style={{
+                                    rotate: rotate,
+                                }}
+                            />
                         </Circle>
                     </div>
                     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                        <Circle className="absolute -left-[600px] -top-[70px]">
-                            <img src="/assets/images/cylinder.png" alt="cylinder 3D image" className="size-[140px]" />
+                        <Circle className="absolute -left-[600px] -top-[70px]" animate>
+                            <motion.img
+                                src="/assets/images/cylinder.png"
+                                alt="cylinder 3D image"
+                                className="size-[140px]"
+                                style={{
+                                    rotate,
+                                }}
+                            />
                         </Circle>
                     </div>
                     <h2 className="font-heading font-black text-4xl md:text-5xl lg:text-6xl text-center">
