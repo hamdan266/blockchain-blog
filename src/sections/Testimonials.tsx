9dfd2929a42557277f96005cacd5299d2896fb9d
hamdan266 <sehahh26@gmail.com>
2024-09-20 00:09:37 +0700

    testimonials animation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,4 +1,5 @@
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
 
 const testimonials = [
     {
@@ -28,7 +29,28 @@ export const TestimonialsSection = () => {
             <div className="container">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 md:gap-8 lg:gap-12">
                     {testimonials.map((testimonial, testimonialIndex) => (
-                        <blockquote key={testimonialIndex} className={twMerge(testimonialIndex === 2 && "md:hidden lg:block")}>
+                        <motion.blockquote
+                            key={testimonialIndex}
+                            initial={{
+                                opacity: 0,
+                                y: 24
+                            }}
+                            whileInView={{
+                                opacity: 1,
+                                y: 0,
+                            }}
+                            viewport={{
+                                once: true,
+                            }}
+                            transition={{
+                                delay: testimonialIndex * 0.5,
+                                ease: "easeInOut",
+                                duration: 1,
+                            }}
+                            className={twMerge(
+                                testimonialIndex === 2 && "md:hidden lg:block"
+                            )}
+                        >
                             <p className="font-heading text-3xl lg:text-4xl font-black">
                                 &ldquo;{testimonial.text}&rdquo;
                             </p>
@@ -51,7 +73,7 @@ export const TestimonialsSection = () => {
                                     </div>
                                 </div>
                             </cite>
-                        </blockquote>
+                        </motion.blockquote>
                     ))}
                 </div>
             </div>
